9837c09aefb57e1a05b5dd166a8b8e7c81afebbe
FireLord1 <firelord.xda@gmail.com>
2016-07-07 23:03:32 +0530

    merlin: xosp-ify

--- xosp.mk
+++ xosp.mk
@@ -14,13 +14,14 @@
 
 $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+# Inherit XOSP common stuff
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
+
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := cm_merlin
+PRODUCT_NAME := xosp_merlin
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := merlin
