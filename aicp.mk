11c20c177896127f67a83fb002c1e2debb00c436
FireLord1 <firelord.xda@gmail.com>
2016-07-07 22:21:53 +0530

    merlin: aicp-ify

--- aicp.mk
+++ aicp.mk
@@ -14,13 +14,21 @@
 
 $(call inherit-product, device/motorola/merlin/full_merlin.mk)
 
+# Inherit AICP common stuff
+$(call inherit-product, vendor/aicp/configs/common.mk)
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        DEVICE_MAINTAINERS="FireLord"
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
+-include vendor/aicp/configs/bootanimation.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := merlin
-PRODUCT_NAME := cm_merlin
+PRODUCT_NAME := aicp_merlin
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := merlin
